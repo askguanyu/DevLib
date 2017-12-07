50388c7c9e96dbb61346f52d1c02a99a5f5681f5
yuguan <yuguan>
2017-12-06 20:01:18 -0500

    1 [INT] initial commit;
    2 [NEW] add folder structure;
    3 [NEW] add solution template;

---
+++ Build/NuGet/DevLib.ServiceBus.Publish.Debug.bat
@@ -0,0 +1,39 @@
+@echo off
+
+set "PROJ=DevLib.ServiceBus"
+set "CONFIG=Debug"
+
+@echo --------------------------------
+@echo NuGet start to pack...
+
+nuget pack "..\..\Source\%PROJ%\%PROJ%.csproj" -Properties "Configuration=%CONFIG%" -OutputDirectory "..\..\Source\%PROJ%\bin\%CONFIG%"
+
+
+@echo --------------------------------
+@echo List nupkg files...
+
+dir "..\..\Source\%PROJ%\bin\%CONFIG%\*.nupkg"
+
+for /f "delims=" %%x in ('dir /od /b "..\..\Source\%PROJ%\bin\%CONFIG%\%PROJ%*.nupkg"') do set LATEST=%%x
+
+if %Errorlevel% NEQ 0 goto End
+
+
+@echo --------------------------------
+
+choice /M "Do you want to publish %LATEST% ?"
+
+if Errorlevel 2 goto No
+if Errorlevel 1 goto Yes
+
+goto End
+
+:No
+goto End
+
+:Yes
+nuget push "..\..\Source\%PROJ%\bin\%CONFIG%\%LATEST%" %1 -Verbosity detailed
+
+:End
+@echo --------------------------------
+@echo Done!
